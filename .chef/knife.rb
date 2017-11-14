a97208d0772d1b3c860745fb08e010e00935b6fc
SanjayKumarC <sanjayklu93@gmail.com>
2017-11-14 10:43:05 +0000

    sixth commit

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,12 @@
 log_level                :info
 log_location             STDOUT
 node_name                'sanjay'
-client_key               '~/chef-repo/.chef/sanjay.pem'
+client_key               '/root/chef-repo/.chef/sanjay.pem'
 validation_client_name   'sanjay-validator'
-validation_key           '~/chef-repo/.chef/sanjayorg.pem'
-chef_server_url          'https://123.45.67.89/organizations/sanjay'
-syntax_check_cache_path  '~/chef-repo/.chef/syntax_check_cache'
+validation_key           '/root/chef-repo/.chef/sanjayorg.pem'
+chef_server_url          'https://ec2-34-216-42-72.us-west-2.compute.amazonaws.com/organizations/sanjayc'
+syntax_check_cache_path  '/root/chef-repo/.chef/syntax_check_cache'
 cookbook_path [ '~/chef-repo/cookbooks' ]
+
+knife[:aws_access_key_id] = "AKIAIIC425AONDSQG4SQ"
+knife[:aws_secret_access_key] = "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"
